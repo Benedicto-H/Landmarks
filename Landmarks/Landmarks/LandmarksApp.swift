63f732ad77c37e1b2d016eec762941861d0ade91
JP <wlsvy1005@gmail.com>
2023-04-29 18:44:38 +0900

    Initialization Landmarks

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 홍진표 on 2023/04/29.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
